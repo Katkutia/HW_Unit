7a08ef466c55bd982a77112a5b9cc4a13a95efcb
Katerina Platonova <platonovihome@gmail.com>
2024-03-26 16:09:30 +0300

    cheange src/test/java/ru/netology/service/CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,39 +1,38 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
+import ru.netology.service.CashbackHackService;
 
 import static org.junit.Assert.assertEquals;
-
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void testRemain(int expected) {
+    public void testRemain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
-        int exspected = 0;
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
 
     @Test
-    public void testRemainWitCashback(int expected) {
+    public void testRemainWitCashback() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
-        int exspected = 100;
+        int expected = 100;
 
         assertEquals(actual, expected);
     }
 
     @Test
-    public void testRemainCashbackMin(int expected) {
+    public void testRemainCashbackMin() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int actual = service.remain(amount);
-        int exspected = 1000;
+        int expected = 1000;
 
         assertEquals(actual, expected);
     }
